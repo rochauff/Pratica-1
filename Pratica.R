e4aab5fa499284fe7bc224bddfd90a23e76862b3
rochauff <marcelorcr@id.uff.br>
2025-06-09 09:47:00 -0300

    Add files via upload

---
+++ Pratica.R
@@ -0,0 +1,6 @@
+library(tidyverse)
+library(readxl)
+base1 <- read_excel("Base_tuberculose.xls")
+base2 <- read_excel("Base_indicadores.xls")
+
+base_final <- full_join(base1, base2, by = c("CODIBGE_com6", "Municipio"))
